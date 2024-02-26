8637ebe157a37155215b4fae20c0fbdcac0d02b1
ethannance <ethannance523@gmail.com>
2024-02-26 16:20:12 -0700

    Modified controllers

--- Mission08_Team0410/Controllers/HomeController.cs
+++ Mission08_Team0410/Controllers/HomeController.cs
@@ -8,13 +8,6 @@ namespace Mission08_Team0410.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
